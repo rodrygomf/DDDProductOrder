010285fff8116457f7601a25e8262962ed9f9084
Rodrigo Figueiredo <rodrigomf@ymail.com>
2022-09-28 19:52:40 -0300

    Add events to project

---
+++ src/domain/event/customer/handler/handler/envia-console-log.handler.ts
@@ -0,0 +1,14 @@
+import EventHandlerInterface from "../../../@shared/event-handler.interface";
+import CustomerAddressChangedEvent from "../../customer-address-changed.event";
+
+export default class EnviaConsoleLogHandler
+  implements EventHandlerInterface<CustomerAddressChangedEvent>
+{
+  handle(event: CustomerAddressChangedEvent): void {
+    console.log(
+      `Endereço do cliente: ${event.eventData.id}, ${
+        event.eventData.name
+      } alterado para: ${JSON.stringify(event.eventData.address)}`
+    );
+  }
+}
